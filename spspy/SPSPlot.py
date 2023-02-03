Fix a few potential bugs in reaction by better separating UI parameters from calculation parameters.

spspy/SPSPlot.py
@@ -4,6 +4,8 @@ from .data.NuclearData import *
 from dataclasses import dataclass, field
 import csv
 
+DEG2RAD: float = np.pi / 180.0
+
 @dataclass
 class Excitation:
     excitation: float = 0.0 #MeV
@@ -45,7 +47,7 @@ class SPSPlot:
         
     def update_reactions(self) -> None:
         for datum in self.data.values():
-            datum.rxn.update_parameters(self.beamEnergy, self.spsAngle, self.magneticField)
+            datum.rxn.update_parameters(self.beamEnergy, self.spsAngle * DEG2RAD, self.magneticField)
             if datum.rxn.targetMaterial.name in self.targets:
                 datum.rxn.targetMaterial = self.targets[datum.rxn.targetMaterial.name]
             for ex in datum.excitations:

spspy/data/NuclearData.py
@@ -24,7 +24,7 @@ def generate_nucleus_id(z: np.uint32, a: np.uint32) -> np.uint32 :
     return z*z + z + a if z > a else a*a + z
 
 class NuclearDataMap:
-    U2MEV: float = 931.4940954
+    U2MEV: float = 931.493614838475
     ELECTRON_MASS: float = 0.000548579909
 
     def __init__(self):