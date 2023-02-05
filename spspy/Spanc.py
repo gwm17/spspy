Fix bug in Spanc where angles weren't being converted to radians. Fix rendering of compunds in Spanc target table

spspy/Spanc.py
@@ -7,6 +7,7 @@ import numpy as np
 from enum import Enum
 
 INVALID_PEAK_ID: int = -1
+DEG2RAD: float = np.pi / 180.0
 
 class PeakType(Enum):
     CALIBRATION = "Calibration"
@@ -58,6 +59,7 @@ class Spanc:
             print("Cannot create reaction with non-existant target ", targetName)
             return
         key = f"Rxn{len(self.reactions)}"
+        params.spsAngle *= DEG2RAD
         rxn = Reaction(params, target=self.targets[targetName])
         self.reactions[key] = rxn
 
@@ -65,7 +67,7 @@ class Spanc:
         if rxnName in self.reactions:
             rxn = self.reactions[rxnName]
             rxn.params.beamEnergy = beamEnergy
-            rxn.params.spsAngle = spsAngle
+            rxn.params.spsAngle = spsAngle * DEG2RAD
             rxn.params.magneticField = magneticField
 
     def add_calibration(self, data: Peak) -> None: